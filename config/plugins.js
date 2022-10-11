935098862a79833082da42cda1d5a91b1fbf3ec4
thomasschwinn <thomas.schwinn@koeln.de>
2022-10-11 11:06:12 +0200

    add cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: "dqab3cb6x",
+        api_key: "274232458722589",
+        api_secret: "kboQkrgG99OCdfqLCcqpHzf_nK4",
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
